6b4fcacc15e668cf65be13ce7caeafa3ef5936ed
einstein.qn@gmail.com <einstein.qn@gmail.com@ee9533d4-0b23-7328-a3c6-0da23eb89522>
2015-03-09 16:51:08 +0000

    source

--- source/luanvan/src/com/example/luanvan/MainActivity.java
+++ source/luanvan/src/com/example/luanvan/MainActivity.java
@@ -8,11 +8,16 @@ import com.example.luanvan.game.Game;
 import com.example.luanvan.mainmenu.MainMenu;
 
 import android.R.color;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -22,66 +27,76 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	TextView tv = null ;
+	TextView tv = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		RelativeLayout layout = (RelativeLayout) findViewById(R.id.layount);
-		GamePanel g = new GamePanel(getBaseContext());
-		g.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		GamePanel g = new GamePanel(this);
+		g.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT));
 		layout.addView(g);
-		
-//		tv = new TextView(this);
-//		loadDoc();
-//		tv.setX(10);
-//		tv.setY(10);
-//		tv.setHeight(100);
-//		tv.setWidth(50);
-//		
-//		layout.addView(tv);
-		
-//		
-//		
-//		
-//		Game game = new Game(this);
-//		game.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-//		
-//		layout.addView(game);
-//		
-//		Button btn = new Button(this);
-//		btn.setLayoutParams(new LayoutParams(100, 30 ));
-//		btn.setBackgroundResource(R.drawable.main_menu_button_bat_dau);
-//		btn.setX(150);
-//		btn.setY(200);
-//		layout.addView(btn);
+
+		// tv = new TextView(this);
+		// loadDoc();
+		// tv.setX(10);
+		// tv.setY(10);
+		// tv.setHeight(100);
+		// tv.setWidth(50);
+		//
+		// layout.addView(tv);
+
+		//
+		//
+		//
+		// Game game = new Game(this);
+		// game.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+		// LayoutParams.MATCH_PARENT));
+		//
+		// layout.addView(game);
+		//
+		// Button btn = new Button(this);
+		// btn.setLayoutParams(new LayoutParams(100, 30 ));
+		// btn.setBackgroundResource(R.drawable.main_menu_button_bat_dau);
+		// btn.setX(150);
+		// btn.setY(200);
+		// layout.addView(btn);
 		
 	}
-	
-	private void loadDoc(){
-		
-        String s = "";
 
-        for(int x=0;x<=100;x++){
-            s += "Line: "+String.valueOf(x)+"\n";
-        }
+	private void loadDoc() {
 
-        
+		String s = "";
+
+		for (int x = 0; x <= 100; x++) {
+			s += "Line: " + String.valueOf(x) + "\n";
+		}
+
 		tv.setMovementMethod(new ScrollingMovementMethod());
 
-        tv.setText(s);
+		tv.setText(s);
 
-    }
+	}
+
+	public void startActivitySync() {
+		Intent i = new Intent(this, SyncActivity.class);
+		startActivity(i);
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		
+
 		getMenuInflater().inflate(R.menu.main, menu);
-		
+
 		return true;
 	}
+
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
